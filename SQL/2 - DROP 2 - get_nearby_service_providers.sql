cfffc7a2554dafb16c201b3019eb0ceae0e7f56f
Kaspar L. Palgi <git@e-stonia.co.uk>
2023-07-12 01:55:37 +0300

    Update

--- SQL/2 - DROP 2 - get_nearby_service_providers.sql
+++ SQL/2 - DROP 2 - get_nearby_service_providers.sql
@@ -1,1 +1,2 @@
+-- If something goes wrong then delete the old stuff before creating new
 DROP FUNCTION IF EXISTS public.get_nearby_service_providers(double precision, double precision, integer);
--- SQL/2 - get_nearby_service_providers.sql
+++ SQL/2 - get_nearby_service_providers.sql
@@ -7,24 +7,9 @@ BEGIN
 RETURN QUERY
 SELECT
   sp.id,
-  sp.status,
-  sp.sort,
-  sp.date_created,
-  sp.user_updated,
-  sp.date_updated,
-  sp."user",
-  sp.services,
-  sp.experience_desc,
-  sp.travel_distance,
-  sp.experience,
-  sp.own_transport,
-  sp.max_weight,
-  sp.provider_description,
-  sp.time_start,
-  sp.time_end,
-  sp.national_id,
-  sp.emergency_transport,
+  sp.first_name,
   sp.geo_location,
+  <---- ALL THE OTHER FIELDS I HAVE IN THE TABLE ---->
   ST_Distance(
     ST_SetSRID(sp.geo_location, 4326)::geography,
     ST_SetSRID(ST_MakePoint(lon, lat), 4326)::geography
